60b98cd3de6806f8bd48446635c04de9a00d80bd
Muhammad Ahmed <96483397+Aplha-pro@users.noreply.github.com>
2024-06-01 02:35:22 +0500

    Update index2.js

--- DOM Tasks/index2.js
+++ DOM Tasks/index2.js
@@ -9,3 +9,8 @@ btn.style.backgroundColor = 'red';
 btn.style.color = 'white';
 
 document.querySelector('body').prepend(btn);
+
+//Create a <p> tag in html, give it a class and some styling.
+//Now create new class in CSS and try to append this class to the <p> element
+//Did you notice, how you overwrite the class name when you add a new one?
+//Solve this problem using classList.
