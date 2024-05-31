fbba5645bbe689d9452c5f4b8ff6a60e99585fe8
Muhammad Ahmed <96483397+Aplha-pro@users.noreply.github.com>
2024-05-31 21:03:28 +0500

    git add

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -1,2 +1,8 @@
 let heading = document.getElementById('heading-1');
-console.log(heading);   
+let classname = document.getElementsByClassName('para-1');
+
+console.log(heading);
+console.dir(heading);
+
+console.log(classname);
+console.dir(classname);
