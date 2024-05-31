00f12b2eac727dbcb1e455e1fded50c9d9c3e5bd
Muhammad Ahmed <96483397+Aplha-pro@users.noreply.github.com>
2024-05-31 22:20:11 +0500

    git - add

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -12,3 +12,10 @@ let h2 = document.querySelector("h2");
 h2.innerText = h2.innerText + ' from Muhammad Ahmed';
 console.log(h2); 
 
+//Task 2
+let divs = document.querySelectorAll('.box');
+console.log(divs);
+divs[0].innerText = "Div-1 Updated";
+divs[1].innerText = "Div-2 Updated";
+divs[2].innerText = "Div-3 Updated";
+console.log(divs);
